131818a15960392b141158e7430e8e4fd6d29311
alpercubukcu <alpercubukcuu@gmail.com>
2024-11-24 12:53:55 +1100

    Add project files.

---
+++ Presentation/UI/Presentation.UI.PanelUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Presentation.UI.PanelUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
