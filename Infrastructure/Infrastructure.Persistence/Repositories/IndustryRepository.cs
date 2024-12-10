46b6991752b1aac6aadd534611269c5793a8fa2f
alpercubukcu <alpercubukcuu@gmail.com>
2024-12-10 20:22:42 +1100

    missing repositories added

---
+++ Infrastructure/Infrastructure.Persistence/Repositories/IndustryRepository.cs
@@ -0,0 +1,21 @@
+﻿using Core.Application.Attributes;
+using Core.Application.Interfaces.Repositories;
+using Core.Domain.Entities;
+using Infrastructure.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    [AddScopedService(Interface = "IIndustryRepository")]
+    public class IndustryRepository : GenericRepository<Industry>, IIndustryRepository
+    {
+        public IndustryRepository(DatabaseContext context) : base(context)
+        {
+            
+        }
+    }
+}
---
+++ Infrastructure/Infrastructure.Persistence/Repositories/ProjectCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Application.Attributes;
+using Core.Application.Dtos;
+using Core.Application.Interfaces.Repositories;
+using Core.Domain.Entities;
+using Infrastructure.Persistence.Context;
+
+
+namespace Infrastructure.Persistence.Repositories
+{
+    [AddScopedService(Interface = "IProjectCategoryRepository")]
+    public class ProjectCategoryRepository : GenericRepository<ProjectCategory>, IProjectCategoryRepository
+    {
+        public ProjectCategoryRepository(DatabaseContext context) : base(context)
+        {
+            
+        }
+    }
+}
