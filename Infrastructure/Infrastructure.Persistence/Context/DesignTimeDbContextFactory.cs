fe59ea2df1b7020c661c7adf50250dcfabe98551
Alper Cubukcu <107555262+alpercubukcuu@users.noreply.github.com>
2025-06-02 15:02:22 +1000

    Update DesignTimeDbContextFactory.cs

--- Infrastructure/Infrastructure.Persistence/Context/DesignTimeDbContextFactory.cs
+++ Infrastructure/Infrastructure.Persistence/Context/DesignTimeDbContextFactory.cs
@@ -10,7 +10,7 @@ namespace Infrastructure.Persistence.Context
         public DatabaseContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
-            optionsBuilder.UseSqlServer("Server=212.81.47.153,1433;Database=Boostionic;User Id=dev_user;Password=Sivax.34;TrustServerCertificate=True");
+            optionsBuilder.UseSqlServer("your-connettion");
 
             return new DatabaseContext(optionsBuilder.Options);
         }
